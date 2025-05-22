835c931efadc52fb9bc0481d67b74a31f3087906
francisco-josé-vieira <franciscojose2703@gmail.com>
2025-05-22 14:21:51 -0300

    Tabela de transação criada

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,12 +1,40 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 
+import { PriceHightLight, TransactionsContainer, TransactionsTable } from "./styles";
+
 export function Transactions() {
   return (
-    
     <div>
-        <Header />
-        <Summary />
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHightLight variant="income">
+                  R$ 12.000,00
+                </PriceHightLight>
+              </td>
+              <td>Venda</td>
+              <td>22/05/2025</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHightLight variant="outcome">
+                  -R$ 50,00
+                </PriceHightLight>
+              </td>
+              <td>Alimentação</td>
+              <td>22/05/2025</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
-  )
+  );
 }
