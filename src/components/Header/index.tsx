6631c2a28319a036f9fedddc65999effa16db68e
francisco-josé-vieira <franciscojose2703@gmail.com>
2025-05-25 19:07:16 -0300

    Modal de transações concluído

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,8 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImg from '../../assets/Logo.svg'
+import * as Dialog from "@radix-ui/react-dialog";
+import { Flex, Text, Button, TextField } from "@radix-ui/themes";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
@@ -7,7 +10,21 @@ export function Header() {
         <HeaderContent>
             <img src={logoImg} alt="" />
 
+
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+          
             <NewTransactionButton>Nova Transação</NewTransactionButton>
+
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+
+        </Dialog.Root>
+
+
+
         </HeaderContent>
     </HeaderContainer>
   )
